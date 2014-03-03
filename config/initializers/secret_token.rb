0782425cfb2a157a32381f053db115ca7f6abbe9
kremez19 <kremez19@gmail.com>
2014-03-03 12:04:31 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaperModels::Application.config.secret_key_base = '39e969859a694f710e501cfaa6722839f2e8be42b9393fda7d1bb42d92a6f72f9d93f64935ab009a574b0c4944b549a787f8250f3d1ba966c9522fffefde88db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PaperModels::Application.config.session_store :cookie_store, key: '_paper_models_session'
